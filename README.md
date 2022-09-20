979fe36f616dc6f7e465f2ca22b1a836d42f7caa
NoahWest <noahwestberry@gmail.com>
2022-09-19 21:14:16 -0700

    rewrote index as i could not recall what i was doing prior, and added more to README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Professional README Generator Starter Code
+# Professional README Generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+## Description
+
+This is a command line interface application which creates a README file for your application repositories. This will allow people to understand how your app works, how to install it, and how other developers can contribute to your project. This README.md is being created via this system.
+
+## Questions
+      
+  _For further questions:_
+
+  If you have any questions, or additional feedback, please feel free to email me and I will respond as soon as possible.
+  
+  _Contact Info:_
+
+  GitHub: [NoahWestTech](https://github.com/noahwesttech)
+
+  Email: [NoahWestberry@Gmail.com](mailto:noahwestberry@gmail.com)
--- index.js
+++ index.js
@@ -1,96 +1,148 @@
-// Packages needed for this application
+// Global require's
+const inquirer = require('inquirer');
 const fs = require('fs');
-const inquirer = import('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./src/generateMarkdown.js')
 
-// TODO: Create an array of questions for user input
+// Array of questions to ask the user
 const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'To begin, please input your full name:',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Please enter your full name');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub username:',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please input your GitHub username so people can find your other work');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Please enter your email address so people know how to contact you')
-                return false;
-            }
-        }
-    },
+    // Project name
     {
         type: 'input',
         name: 'title',
-        message: 'Enter the title of your project',
+        message: 'What is the title of the project? (Required)',
         validate: titleInput => {
             if (titleInput) {
                 return true;
             } else {
-                console.log('Please enter the title of your project');
+                console.log('You need to enter a title to continue!');
                 return false;
             }
         }
-    }
+    },
+    // Description of project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to provide a project description!');
+                return false;
+            }
+        }
+    },
+    // Installation Instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project? (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue!');
+                return false;
+            }
+        }
+    },
+    // Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use project!');
+                return false;
+            }
+        }
+    },
+    // Contribution Guidlines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this project? (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the project!');
+                return false;
+            }
+        }
+    },
+    // Test Instructions 
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project? (Required)',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this project!');
+                return false;
+            }
+        }
+    },
+    // License Options
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project (Required)',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project!');
+                return false;
+            }
+        }
+    },
+    // Github Username
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    // Email Address
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Would you like to include your email?',
+    },
 ];
 
-// TODO: Create a function to write README file
-const writeToFile = data => {
-    return new Promise((resolve, reject) => {
-        // make a readme file and add to dist folder
-        fs.writeFile('./dist/README.md', data, err => {
-            // if there's an error, reject the Promise and send the error to .catch() method
-            if (err) {
-                reject (err);
-                // return out of the function here to make sure the Promise doesn't continut to execute the resolve() function
-                return;
-            }
-            // if everything went well, resolve the Promise and send the successful data to the .then() method
-            resolve({
-                ok: true,
-                message: console.log('Success! Navigate to the "dist" folder to see your README!')
-            });
-        })
-    })
-}
-// TODO: Create a function to initialize app
-const init = () => {
-    return inquirer.prompt(questions);
-}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Success! Information transferred to the README!')
+    });
+};
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
-init()
-.then(userInput => {
-    return generateMarkdown(userInput);
-})
-.then(readmeInfo => {
-    return writeToFile(readmeInfo);
-})
-.catch(err => {
-    console.log(err);
-})
+init();
